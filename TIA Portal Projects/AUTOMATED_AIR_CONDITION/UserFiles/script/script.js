7a9e36c90a9bc0d95ecbb2c8b2fd3e29a67688a9
AlperenCevikUU <100619766+AlperenCevikUU@users.noreply.github.com>
2022-10-09 18:41:54 +0300

    projects

---
+++ TIA Portal Projects/AUTOMATED_AIR_CONDITION/UserFiles/script/script.js
@@ -0,0 +1,48 @@
+var Q0_0;
+
+$(document).ready(function () {
+    $.ajaxSetup({ cache: false });
+    setInterval(function () {
+        $.getJSON("s7.html", function (result) {
+            Q0_0 = result["q0_0"];
+            if (Q0_0 == 1) {
+                document.getElementById("state1").src = "./images/close.png";
+            }
+            else {
+                document.getElementById("state1").src = "./images/open.png";
+            }
+            document.getElementById("demoGauge2_1").style.setProperty('--gauge-display-value', result["md12"]);
+            document.getElementById("demoGauge2_1").style.setProperty('--gauge-value', result["md12"]);
+
+        });
+    }, 500);
+});
+
+
+function on1() {
+    ulr = 's7.html';
+    name = '"Tag_18"';
+    sdata = escape(name) + '=' + 1;
+    $.post(ulr, sdata, function (result2) { });
+}
+
+function release_on1(){
+    ulr = 's7.html';
+    name = '"Tag_18"';
+    sdata = escape(name) + '=' + 0;
+    $.post(ulr, sdata, function (result2) { });
+}
+
+function off1() {
+    ulr = 's7.html';
+    name = '"Tag_19"';
+    sdata = escape(name) + '=' + 1;
+    $.post(ulr, sdata, function (result2) { });
+}
+
+function release_off1(){
+    ulr = 's7.html';
+    name = '"Tag_19"';
+    sdata = escape(name) + '=' + 0;
+    $.post(ulr, sdata, function (result2) { });
+}
